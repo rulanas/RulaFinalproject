76ec07990466762ac508b77ac27a808f16132347
rulanas1 <rulanas1@gmail.com>
2017-09-26 12:31:50 +0300

    hhh

--- app/src/main/java/com/example/hp1/finalproject/MainActivity.java
+++ app/src/main/java/com/example/hp1/finalproject/MainActivity.java
@@ -1,13 +1,44 @@
 package com.example.hp1.finalproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    EditText username, password;
+    Button signup, login;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        username=(EditText)findViewById(R.id.username);
+        password=(EditText)findViewById(R.id.password);
+        signup=(Button)findViewById(R.id.signup);
+        login=(Button)findViewById(R.id.login);
+        signup.setOnClickListener(this);
+        login.setOnClickListener(this);
+
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v==signup){
+            Intent i=new Intent(this,Main3Activity.class);
+            startActivity(i);
+        }
+        if(v==login){
+            Intent i1=new Intent(this,Main2Activity.class);
+            startActivity(i1);
+        }
     }
 }
