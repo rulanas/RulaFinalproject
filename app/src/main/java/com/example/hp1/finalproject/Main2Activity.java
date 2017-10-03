69e73781b011bd2080e445084c4708253143e212
rulanas1 <rulanas1@gmail.com>
2017-10-03 12:30:09 +0300

    ll

--- app/src/main/java/com/example/hp1/finalproject/Main2Activity.java
+++ app/src/main/java/com/example/hp1/finalproject/Main2Activity.java
@@ -2,12 +2,37 @@ package com.example.hp1.finalproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
-public class Main2Activity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class Main2Activity extends AppCompatActivity implements AdapterView.OnItemClickListener{
+
+    ListView lvItems;
+    ArrayList<Item> items = new ArrayList<Item>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        lvItems=(ListView)findViewById(R.id.lvItems);
+
+        items.add(new Item("Third Item",R.drawable.settings));
+        items.add(new Item("Third Item",R.drawable.fo));
+        items.add(new Item("Third Item",R.drawable.spor));
+        items.add(new Item("Third Item",R.drawable.ch));
+        items.add(new Item("Third Item",R.drawable.info));
+
+
+        CustomAdapter adapter = new CustomAdapter(this, R.layout.custom_row, items);
+        lvItems.setAdapter(adapter);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
     }
 }
