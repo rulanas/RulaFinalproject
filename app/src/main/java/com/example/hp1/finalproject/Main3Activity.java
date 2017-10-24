8bfafe44ad3b6b6a63cd8b1709355738c4bbdb19
rulanas1 <rulanas1@gmail.com>
2017-10-24 11:16:42 +0300

    ll

--- app/src/main/java/com/example/hp1/finalproject/Main3Activity.java
+++ app/src/main/java/com/example/hp1/finalproject/Main3Activity.java
@@ -1,25 +1,32 @@
 package com.example.hp1.finalproject;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 
 public class Main3Activity extends AppCompatActivity implements View.OnClickListener{
     private Bitmap bitmap;
 
     private ImageView imageView;
-    Button btCamera,btGalery;
+    Button btCamera,btGalery, submit;
+    EditText name, pass3,height, age ;
 
     static final int SELECT_IMAGE=1;
     static final int TAKE_IMAGE=0;
@@ -29,12 +36,19 @@ public class Main3Activity extends AppCompatActivity implements View.OnClickList
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
 
+        height =(EditText) findViewById(R.id.height);
+        pass3 =(EditText) findViewById(R.id.pass3);
+        age =(EditText) findViewById(R.id.age);
+        name =(EditText) findViewById(R.id.name);
+        submit =(Button) findViewById(R.id.submit);
         btCamera =(Button) findViewById(R.id.btCamera);
         btGalery = (Button) findViewById(R.id.btGalery);
         imageView=(ImageView) findViewById(R.id.imageView);
 
         btCamera.setOnClickListener(this);
         btGalery.setOnClickListener(this);
+        submit.setOnClickListener(this);
+
 
         if(!hasCamera()){
             btCamera.setEnabled(false);
@@ -45,34 +59,58 @@ public class Main3Activity extends AppCompatActivity implements View.OnClickList
     }
     @Override
     public void onClick(View v) {
+
+       if(v==submit) {
+           String user = name.getText().toString(), pass = pass3.getText().toString(), old = age.getText().toString(),
+                   heightt = height.getText().toString();
+
+           if ((!user.equals("")) && (!pass.equals("")) && (!old.equals("") && (!heightt.equals("")))) {
+               Intent i4 = new Intent(this, Main2Activity.class);
+               startActivity(i4);
+           } else {
+               AlertDialog.Builder builder = new AlertDialog.Builder(this);
+               builder.setCancelable(true);
+               builder.setTitle("login");
+               builder.setMessage("you missed something");
+               builder.setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                   @Override
+                   public void onClick(DialogInterface dialog, int which) {
+
+                   }
+               });
+               builder.create().show();
+           }
+       }
+
         if(v==btCamera){
             //start another activity and receive a result back in case the activity exists return 0
             Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
             startActivityForResult(i,TAKE_IMAGE );
-        }else{
+        }if(v==btGalery){
             Intent i = new Intent(Intent.ACTION_PICK,
                     android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
             startActivityForResult(i, SELECT_IMAGE);
         }
     }
+
     /**
      *  The resultCode will be RESULT_CANCELED if the activity explicitly returned that, didn't return any result, or crashed during its operation.
      */
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if(requestCode == TAKE_IMAGE && resultCode == RESULT_OK){
+        if (requestCode == TAKE_IMAGE && resultCode == RESULT_OK) {
             Bundle extra = data.getExtras();
-            bitmap  = (Bitmap) extra.get("data");
+            bitmap = (Bitmap) extra.get("data");
+            saveImage(bitmap);
             imageView.setImageBitmap(bitmap);
-        }
-        else if(requestCode == SELECT_IMAGE && resultCode == RESULT_OK)
-        {
+        } else if (requestCode == SELECT_IMAGE && resultCode == RESULT_OK) {
             Uri targetUri = data.getData();
             Toast.makeText(getApplicationContext(), targetUri.toString(), Toast.LENGTH_LONG).show();
             Bitmap bitmap;
             try {
 
                 bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(targetUri));
+
                 imageView.setImageBitmap(bitmap);
             } catch (FileNotFoundException e) {
                 // TODO Auto-generated catch block
@@ -80,5 +118,27 @@ public class Main3Activity extends AppCompatActivity implements View.OnClickList
             }
         }
     }
-}
+
+        public File saveImage(Bitmap bitmap){
+
+        File root = Environment.getExternalStorageDirectory();
+        File file = new File(root.getAbsolutePath()+"/DCIM/Camera/img.jpg");
+        try
+        {
+            file.createNewFile();
+            FileOutputStream ostream = new FileOutputStream(file);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, ostream);
+            ostream.close();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            Toast.makeText(this,"Failed to save image, try again",Toast.LENGTH_LONG).show();
+        }
+        return file;
+    }
+
+    }
+
+
 
